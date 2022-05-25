adb9e72f91968b1a66a5db1742f9b2f8b05793d8
f3ve <free.eve@pm.me>
2022-05-25 02:12:42 -0800

    Created  custom link component

--- src/components/FLink.vue
+++ src/components/FLink.vue
@@ -1,7 +1,13 @@
-<script setup></script>
+<script setup>
+// eslint-disable-next-line no-undef
+defineProps(['href', 'to']);
+</script>
 
 <template>
-  <div></div>
+  <a :href="href" v-if="href">
+    <slot />
+  </a>
+  <<router-link v-if="to" :to="to"></router-link>
 </template>
 
 <style scoped></style>
